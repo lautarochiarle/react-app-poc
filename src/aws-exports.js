9cb142f9231d50df298b6e9529e9287151225ede
lautaro <lautaro.chiarle@fluxit.com.ar>
2020-06-05 12:27:22 -0300

    add cognito authentication

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+
+const awsConfig = {
+    identityPoolId: 'us-east-1:710b365d-df5f-4a23-adcc-23dedc4e2436',
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_1gWKJOCHb',
+    userPoolWebClientId: '11fq26p6ms9m99i6ol3okifpgb'
+  }
+  
+  export default awsConfig;
